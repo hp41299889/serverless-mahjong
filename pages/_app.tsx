db503e8aaaf680fe29ec175bf3496130cdadbc4e
Danny Lu[呂學霖] <dannylu@nexaiot.com>
2023-07-14 17:35:12 +0800

    feat: add favicon and device viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import "./record.css";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <title>惠打麻將</title>
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 };
 
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@/component/layout";
+import { FC } from "react";
+
+const Home: FC = () => {
+  return <Layout>Home</Layout>;
+};
+
+export default Home;
